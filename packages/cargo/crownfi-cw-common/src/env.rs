75c1de46e8d4754ce076b0662aeb1750584a6ccc
ARitz Cracker <aritz@aritzcracker.ca>
2023-11-30 18:30:29 +0000

    Actually make the clonable envs clonable

--- packages/cargo/crownfi-cw-common/src/env.rs
+++ packages/cargo/crownfi-cw-common/src/env.rs
@@ -3,7 +3,7 @@ use std::{rc::Rc, cell::RefCell};
 use cosmwasm_std::{Env, Deps, DepsMut, Storage, Api, CustomQuery, Empty, QuerierWrapper};
 
 // common immutable-storage trait?
-
+#[derive(Clone)]
 pub struct ClonableEnvInfo<'exec, Q: CustomQuery = Empty> {
 	pub storage: Rc<&'exec dyn Storage>,
 	pub api: Rc<&'exec dyn Api>,
@@ -22,6 +22,7 @@ impl<'exec> ClonableEnvInfo<'exec> {
 }
 
 /// Wraps Env and Deps
+#[derive(Clone)]
 pub struct ClonableEnvInfoMut<'exec, Q: CustomQuery = Empty> {
 	pub storage: Rc<RefCell<&'exec mut dyn Storage>>,
 	pub api: Rc<&'exec dyn Api>,
