063a9b8ae3eb72d6cd694a65bc8611f08938b178
ARitz Cracker <aritz@aritzcracker.ca>
2024-01-19 20:53:50 +0000

    update npm/sei-utils * add checkVersion to BaseContract

--- packages/npm/sei-utils/src/contract_base.ts
+++ packages/npm/sei-utils/src/contract_base.ts
@@ -1,8 +1,39 @@
 import { CosmWasmClient, ExecuteInstruction, SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate"
 import { Addr, ContractVersionInfo } from "./common_sei_types.js";
 import { Coin } from "@cosmjs/amino";
+import semverSatisfies from "semver/functions/satisfies.js";
 
 const CONTRACT_INFO_KEY = Buffer.from("contract_info");
+
+export class ContractVersionNotSatisfiedError extends Error {
+	name!: "ContractVersionNotSatisfiedError"
+	contractAddress: Addr
+	expectedVersions: {[name: string]: string}
+	actualVersionInfo: ContractVersionInfo | null
+	constructor(
+		contractAddress: Addr,
+		expectedVersions: {[name: string]: string},
+		actualVersionInfo: ContractVersionInfo | null
+	){
+		if (actualVersionInfo) {
+			super(
+				"Expected contract " + contractAddress + " to be compatible with " + 
+				Object.keys(expectedVersions).map((name) => {
+					name + "@" + expectedVersions[name]
+				}).join(", ") + "; " +
+				"but the actual contract version information was " +
+				actualVersionInfo.name + "@" + actualVersionInfo.version
+			);
+		}else{
+			super("The contract at " + contractAddress + " has no version information; perhaps it doesn't exist")
+		}
+		
+		this.contractAddress = contractAddress;
+		this.expectedVersions = expectedVersions;
+		this.actualVersionInfo = actualVersionInfo;
+	}
+}
+ContractVersionNotSatisfiedError.prototype.name = "ContractVersionNotSatisfiedError";
 
 /**
  * A class which is usually extended upon to generate a contract API
@@ -27,6 +58,29 @@ export class ContractBase {
 			return null;
 		}
 		return JSON.parse(Buffer.from(storedData.buffer, storedData.byteOffset, storedData.byteLength).toString());
+	}
+	/**
+	 * Resolves the promise if the contract name matches and if the contract version satisfies the semver range
+	 * specified. Otherwise, the promise is rejected with a `ContractVersionNotSatisfiedError`.
+	 * @param versions A map of name => version. e.g. `{"my-awesome-contract": "^1.0.0"}`
+	 */
+	async checkVersion(versions: {[name: string]: string}): Promise<void> {
+		const versionInfo = await this.getVersion();
+		if (versionInfo == null) {
+			throw new ContractVersionNotSatisfiedError(
+				this.address,
+				versions,
+				versionInfo
+			);
+		}
+		const expectedVersion = versions[versionInfo.name];
+		if (!expectedVersion || !semverSatisfies(versionInfo.version, expectedVersion)) {
+			throw new ContractVersionNotSatisfiedError(
+				this.address,
+				versions,
+				versionInfo
+			);
+		}
 	}
 	/**
 	 * Executes the contracts `query` function with the specified payload encoded as JSON
