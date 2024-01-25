40955af2ea1a062b02f99cda692381e3a4b91aa0
ARitz Cracker <aritz@aritzcracker.ca>
2024-01-25 19:59:37 +0000

    update npm/sei-utils * Add read-only-address provider * allow index to be specified when using wallet seeds

--- packages/npm/sei-utils/src/client_env.ts
+++ packages/npm/sei-utils/src/client_env.ts
@@ -10,8 +10,8 @@ import { MsgExecuteContract } from "cosmjs-types/cosmwasm/wasm/v1/tx.js";
 import { Addr } from "./common_sei_types.js";
 
 // TODO: New provider: "read-only-address"
-export type MaybeSelectedProviderString = KnownSeiProviders | "seed-wallet" | null;
-export type MaybeSelectedProvider = KnownSeiProviders | {seed: string} | null;
+export type MaybeSelectedProviderString = KnownSeiProviders | "seed-wallet" | "read-only-address" | null;
+export type MaybeSelectedProvider = KnownSeiProviders | {seed: string, index?: number} | {address: string} | null;
 
 export class TransactionError extends Error {
 	name!: "TransactionError"
@@ -27,7 +27,10 @@ TransactionError.prototype.name == "TransactionError";
 
 function maybeProviderToMaybeString(provider: MaybeSelectedProvider): MaybeSelectedProviderString {
 	if (typeof provider == "object" && provider != null) {
-		return "seed-wallet";
+		if ("seed" in provider) {
+			return "seed-wallet";
+		}
+		return "read-only-address";
 	}
 	return provider;
 }
@@ -77,7 +80,13 @@ export class ClientEnv {
 			return;
 		}
 		if (typeof defaultProvider == "object" && typeof oldProvider == "object") {
-			if (defaultProvider != null && oldProvider != null && defaultProvider.seed == oldProvider.seed) {
+			if (
+				defaultProvider != null &&
+				oldProvider != null &&
+				(defaultProvider as any).address == (oldProvider as any).address &&
+				(defaultProvider as any).seed == (oldProvider as any).seed &&
+				(defaultProvider as any).index == (oldProvider as any).index
+			) {
 				return;
 			}
 		}
@@ -143,22 +152,32 @@ export class ClientEnv {
 	private static async getSigner(
 		provider: MaybeSelectedProvider,
 		networkConfig: SeiChainNetConfig
-	): Promise<OfflineSigner | string> {
+	): Promise<{signer: OfflineSigner} | {failure: string} | {address: string}> {
 		if (provider == null) {
-			return "No wallet selected";
+			return {
+				failure: "No wallet selected"
+			};
 		}
 		if (typeof provider == "object") {
-			// async imports allow us to load the signing stuff only if needed. (hopefully)
-			const {restoreWallet} = await import("@crownfi/sei-js-core");
-			// Just support the NULL seed for now.
-			return await restoreWallet(provider.seed)
+			if ("seed" in provider) {
+				// async imports allow us to load the signing stuff only if needed. (hopefully)
+				const {restoreWallet} = await import("@crownfi/sei-js-core");
+				return {
+					signer: await restoreWallet(provider.seed, provider.index)
+				};
+			}
+			return {
+				address: provider.address
+			};
 		}
 		const signer = await (new SeiWallet(provider)).getOfflineSigner(networkConfig.chainId);
 		if (signer == undefined) {
-			return KNOWN_SEI_PROVIDER_INFO[provider].name +
-				" did not provide a signer. (Is the wallet unlocked and are we authorized?)";
+			return {
+				failure: KNOWN_SEI_PROVIDER_INFO[provider].name +
+					" did not provide a signer. (Is the wallet unlocked and are we authorized?)"
+			};
 		}
-		return signer;
+		return {signer};
 	}
 
 	static async get<T extends typeof ClientEnv>(
@@ -170,10 +189,22 @@ export class ClientEnv {
 
 		const [wasmClient, account, readonlyReason] = await (async () => {
 			const maybeSigner = await ClientEnv.getSigner(provider, networkConfig);
-			if (typeof maybeSigner == "string") {
-				return [await getCosmWasmClient(networkConfig.rpcUrl), null, maybeSigner];
+			if ("failure" in maybeSigner) {
+				return [await getCosmWasmClient(networkConfig.rpcUrl), null, maybeSigner.failure];
+			}else if ("address" in maybeSigner) {
+				return [
+					await getCosmWasmClient(networkConfig.rpcUrl),
+					{
+						address: maybeSigner.address,
+						// fake data etc.
+						algo: "secp256k1",
+						pubkey: new Uint8Array(0) 
+					} satisfies AccountData,
+					"Address was provided without a seed nor wallet"
+				];
 			}
-			const accounts = await maybeSigner.getAccounts();
+			const {signer} = maybeSigner;
+			const accounts = await signer.getAccounts();
 			if (accounts.length !== 1) {
 				return [
 					await getCosmWasmClient(networkConfig.rpcUrl),
@@ -184,7 +215,7 @@ export class ClientEnv {
 			return [
 				await getSigningCosmWasmClient(
 					networkConfig.rpcUrl,
-					maybeSigner,
+					signer,
 					{
 						gasPrice: GasPrice.fromString("0.1usei")
 					}
@@ -218,10 +249,19 @@ export class ClientEnv {
 		return this.account;
 	}
 	/**
-	 * @returns true if tranasction signing is available
+	 * @returns true if tranasction signing is available and the wallet is known
 	 */
 	isSignable(): this is { wasmClient: SigningCosmWasmClient, account: AccountData } {
 		return (this.wasmClient instanceof SigningCosmWasmClient) && this.account != null;
+	}
+
+	/**
+	 * If you want to actually check if transactions can be sent, use the `isSignable` method
+	 * 
+	 * @returns true if the wallet is known
+	 */
+	hasAccount(): this is {account: AccountData} {
+		return this.account != null;
 	}
 
 	async signAndSend(
@@ -316,8 +356,8 @@ export class ClientEnv {
 		}
 	}
 	execIxsToCosmosMsgs(instructions: ExecuteInstruction[]): MsgExecuteContractEncodeObject[] {
-		if (!this.isSignable()) {
-			throw new Error("Cannot execute transactions - " + this.readonlyReason);
+		if (!this.hasAccount()) {
+			throw new Error("Can't get user wallet address - " + this.readonlyReason);
 		}
 		return instructions.map((i) => {
 			if (i.funds) {
